dc5b2e41f18cfcc3d15d386de3ea3fd3f55e8a72
Unlikely_Hacker <jlm13v@my.fsu.edu>
2018-11-19 09:05:35 -0500

    much addition ideas, now need to impliement them

--- src/findAntiVM.py
+++ src/findAntiVM.py
@@ -7,6 +7,22 @@ for i in heads:
 	if (GetMnem(i) == "sidt" or GetMnem(i) == "sgdt" or GetMnem(i) == "sldt" or GetMnem(i) == "smsw" or GetMnem(i) == "str" or GetMnem(i) == "in" or GetMnem(i) == "cpuid"):
 		antiVM.append(i)
 
+#Calls to specific functions need to be checked: VirtualProtect,GetCursorPosition() 
+'''
+for functionAddr in Functions():    
+    # Check each function to look for strcpy        
+    if "GetCursorPosition" in GetFunctionName(functionAddr):    #Might be worth iterating through a dictionary 
+        xrefs = CodeRefsTo(functionAddr, False)                
+        # Iterate over each cross-reference
+        for xref in xrefs:                            
+            # Check to see if this cross-reference is a function call                            
+            if GetMnem(xref).lower() == "call":           
+                print hex(xref)
+'''
+#Calls to specific registry keys: \Registry\Machine\HARDWARE\ACPI\DSDT\VBOX__\VBOXBIOS,
+#Calls to autorun/autorunsc -- these hide signed MS entries!!
+#Any Modification ot %SystemRoot% directory
+
 print "Number of potential Anti-VM instructions: %d" % (len(antiVM))
 
 for i in antiVM:
