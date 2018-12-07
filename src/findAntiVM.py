4927700d4ebd5abd20d373df47dba93d4669ed36
Jordan Mussman <jlm13v@my.fsu.edu>
2018-12-06 19:26:35 -0500

    Update findAntiVM.py

--- src/findAntiVM.py
+++ src/findAntiVM.py
@@ -2,32 +2,32 @@ from idautils import *
 from idc import *
 
 heads = Heads(SegStart(ScreenEA()), SegEnd(ScreenEA()))
-basic_check = ['sidt', 'sgdt', 'sldt', 'smsw', 'str', 'in', 'cpuid', 'cmd.exe', 'xor']
-vmware_check = ['Vmtoolsd.exe', 'Vmwaretrat.exe', 'Vmwareuser.exe', 'Vmacthlp.exe']
-vbox_check = ['vboxservice.exe', 'vboxtray.exe']
+
+#Calls to autorun/autorunsc -- these hide signed MS entries!!
+basic_check = ['sidt', 'sgdt', 'sldt', 'smsw', 'str', 'in', 'cpuid', 'cmd', 'xor']
+vmware_check = ['Vmtoolsd', 'Vmwaretrat', 'Vmwareuser', 'Vmacthlp']
+vbox_check = ['vboxservice', 'vboxtray', 'VBOXBIOS']
 hostname_check = ['brbrb-d8fb22af1']
-env_check = ['KVMKVMKVM', 'prl hyperv', 'Microsoft Hv']
+env_check = ['KVMKVMKVM', 'prl hyperv', 'Microsoft Hv', 'autorun', 'autorunsc']
 
 antiVM = []
 for i in heads:
         for x in basic_check,vmware_check,vbox_check,hostname_check,env_check:
                 if GetMnem(i) in x: 
 	                antiVM.append(i)
-
+			
+#Check out Hex View-A for I/O ports
 for i in heads:
         if GetMnem(i) == "mov" and "eax" in GetOpnd(i, 0) and GetMnem(i) == "0x564D5868" in GetOpnd(i, 1):
                 if GetMnem(i + 1) == "mov" and "edx" in GetOpnd(i + 1, 0) and GetMnem(i + 1) == "0x5658" in GetOpnd(i + 1, 1):
                         if GetMnem(i + 2) == "in" and "eax" in GetOpnd(i + 2, 0) and GetMnem(i + 2) == "DX" in GetOpnd(i + 2, 1):
-                                antiVM.append(i)
+                                antiVM.append(i) #Should recheck this, might be repeatvitive
                                 antiVM.append(i + 1)
                                 antiVM.append(i + 2)
 
-#Check out Hex View-A for I/O ports
-  #0x564D5868 #VMXh for VMWare I/O port
-  #0x5658 #VX(port) for VMWare I/O port
 
-#Calls to specific functions need to be checked: VirtualProtect,GetCursorPosition() 
 '''
+#Calls to specific functions need to be checked: VirtualProtect,GetCursorPosition() 
 for functionAddr in Functions():    
     # Check each function to look for strcpy        
     if "GetCursorPosition" in GetFunctionName(functionAddr):    #Might be worth iterating through a dictionary 
@@ -38,9 +38,10 @@ for functionAddr in Functions():
             if GetMnem(xref).lower() == "call":           
                 print hex(xref)
 '''
+
 #Calls to specific registry keys: \Registry\Machine\HARDWARE\ACPI\DSDT\VBOX__\VBOXBIOS,
-#Calls to autorun/autorunsc -- these hide signed MS entries!!
 #Any Modification ot %SystemRoot% directory
+#base64 decode - ?
 
 for i in antiVM:
 	SetColor(i, CIC_ITEM, 0x0000ff)
