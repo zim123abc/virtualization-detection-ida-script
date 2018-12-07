dd8015ec6e130cf04272805730c78950b857c02b
Joshua Bittel <joshuabittel123@gmail.com>
2018-12-07 18:46:32 -0500

    fixed a few bugs with searching for instructions...added better printout message

--- src/findAntiVM.py
+++ src/findAntiVM.py
@@ -16,34 +16,27 @@ for i in heads:
         for x in pill_check,vmware_check,vbox_check,hostname_check,env_check,malicious_check:
                 if GetMnem(i) in x: 
 	                antiVM.append(i)
-			
+
+#reset head to beginning of instructions
+heads = Heads(SegStart(ScreenEA()), SegEnd(ScreenEA()))
+
 #Check out Hex View-A for I/O ports
-for i in heads:
-        if GetMnem(i) == "mov" and "eax" in GetOpnd(i, 0) and GetMnem(i) == "0x564D5868" in GetOpnd(i, 1):
-                if GetMnem(i + 1) == "mov" and "edx" in GetOpnd(i + 1, 0) and GetMnem(i + 1) == "0x5658" in GetOpnd(i + 1, 1):
-                        if GetMnem(i + 2) == "in" and "eax" in GetOpnd(i + 2, 0) and GetMnem(i + 2) == "DX" in GetOpnd(i + 2, 1):
-                                antiVM.append(i) #Should recheck this, might be repeatvitive
-                                antiVM.append(i + 1)
-                                antiVM.append(i + 2)
-        if GetMnem(i) == "call" and "RtlGetNativeSystemInformation" in GetOpnd(i, 0):
-                antiVm.append(i)
+for x in heads:
+        if GetMnem(x) == "mov" and "eax" in GetOpnd(x, 0) and "564D5868h" in GetOpnd(x, 1):
+                if GetMnem(x + 1) == "mov" and "edx" in GetOpnd(x + 1, 0) and "5658h" in GetOpnd(x + 1, 1):
+                        if GetMnem(x + 2) == "in" and "eax" in GetOpnd(x + 2, 0) and "dx" in GetOpnd(x + 2, 1):
+                                antiVM.append(x)
 
+        elif GetMnem(x) == "call" and "RtlGetNativeSystemInformation" in GetOpnd(x, 0):
+                antiVM.append(x)
 
-'''
-#Calls to specific functions need to be checked: VirtualProtect,GetCursorPosition() 
-for functionAddr in Functions():    
-    # Check each function to look for strcpy        
-    if "GetCursorPosition" in GetFunctionName(functionAddr):    #Might be worth iterating through a dictionary 
-        xrefs = CodeRefsTo(functionAddr, False)                
-        # Iterate over each cross-reference
-        for xref in xrefs:                            
-            # Check to see if this cross-reference is a function call                            
-            if GetMnem(xref).lower() == "call":           
-                print hex(xref)
-'''
+        if "564D5868h" in GetOpnd(x, 1) or "5658h" in GetOpnd(x, 1):
+                antiVM.append(x)
 
 for i in antiVM:
 	SetColor(i, CIC_ITEM, 0x0000ff)
-	Message("Anti-VM: %08x\n" % i)
+        instruction = GetMnem(i)
+	Message("Anti-VM: %08x >>" % i)
+        Message(" %s\n" % instruction)
         
 print "Number of potential Anti-VM instructions: %d" % (len(antiVM))
